809fff9292330f0a3b5659e325bc4c32186ee039
pyro <pyrotecknical@gmail.com>
2026-03-02 19:05:16 -0800

    change stream announcement to be an embed

--- data/cogs/stream.py
+++ data/cogs/stream.py
@@ -1,29 +1,62 @@
 import aiohttp
+import datetime
 import json
 from discord.ext import commands, tasks
+from discord import Embed
 
 class Stream(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.alreadyLive = True  # initially set to True so that any bot resets while stream is live doesn't send another message
+        self.alreadyLive = False
         with open('secrets.json') as config_file:
             self.config = json.load(config_file)
 
     @tasks.loop(minutes=3)
     async def check_stream(self):
         async with aiohttp.ClientSession() as session:
+            # Get access token
+            token = await self.get_access_token(session)
+
+            # Get stream info
             async with session.get(f'https://api.twitch.tv/helix/streams?user_login={self.config["STREAMER_NAME"]}', headers={
                 'Client-ID': self.config["TWITCH_CLIENT_ID"],
-                'Authorization': f'Bearer {await self.get_access_token(session)}'
+                'Authorization': f'Bearer {token}'
             }) as response:
                 data = await response.json()
+
                 if data['data'] and not self.alreadyLive:
-                    channel = self.bot.get_channel(int(self.config["GOING_LIVE_CHANNEL_ID"]))
-                    if channel:
-                        await channel.send(f'{self.config["STREAMER_NAME"]} is now live! https://www.twitch.tv/{self.config["STREAMER_NAME"]} @everyone')
-                        self.alreadyLive = True
-                    else:
-                        print(f"Channel with ID {self.config['GOING_LIVE_CHANNEL_ID']} not found.")
+                    # Get channel info for thumbnail
+                    async with session.get(f'https://api.twitch.tv/helix/users?login={self.config["STREAMER_NAME"]}', headers={
+                        'Client-ID': self.config["TWITCH_CLIENT_ID"],
+                        'Authorization': f'Bearer {token}'
+                    }) as user_response:
+                        user_data = await user_response.json()
+                        user_info = user_data['data'][0] if user_data['data'] else None
+
+                        print(data['data'][0])
+
+                        if user_info:
+                            # Create embed
+                            embed = Embed(
+                                title=self.config["STREAMER_NAME"],
+                                color=0x9146FF
+                            )
+
+                            # Add stream title and thumbnail
+                            stream_info = data['data'][0]
+                            embed.add_field(name="", value=f"[{stream_info['title']}](https://www.twitch.tv/{self.config["STREAMER_NAME"]})", inline=False)
+                            embed.set_image(url=stream_info['thumbnail_url'].replace('{width}x{height}', '1920x1080'))
+                            embed.set_thumbnail(url=user_info['profile_image_url'])
+                            embed.timestamp = datetime.datetime.now()
+
+                            channel = self.bot.get_channel(int(self.config["GOING_LIVE_CHANNEL_ID"]))
+                            if channel:
+                                await channel.send(f'{self.config["STREAMER_NAME"]} is now live! <https://www.twitch.tv/{self.config["STREAMER_NAME"]}> @everyone', embed=embed)
+                                self.alreadyLive = True
+                            else:
+                                print(f"Channel with ID {self.config['GOING_LIVE_CHANNEL_ID']} not found.")
+                        else:
+                            print("Could not get user info")
                 elif data['data'] and self.alreadyLive:
                     pass
                 else:
