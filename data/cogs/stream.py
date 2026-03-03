4070da49f70ccfcb11176cf4b8cf87d7bb67d1fa
pyro <pyrotecknical@gmail.com>
2026-03-02 20:42:49 -0800

    change from embedding thumbnail link to downloading thumbnail

--- data/cogs/stream.py
+++ data/cogs/stream.py
@@ -1,14 +1,17 @@
 import aiohttp
 import datetime
 import json
+import os
 from discord.ext import commands, tasks
-from discord import Embed
+from discord import Embed, File
 
 class Stream(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.alreadyLive = False
+        self.alreadyLive = True # initially set to True so that messasge doesn't get sent on bot startup if already live
         self.stream_message = None
+        self.stream_start_time = None
+        self.thumbnail_path = "twitch_thumbnail.png"
         with open('secrets.json') as config_file:
             self.config = json.load(config_file)
 
@@ -18,7 +21,7 @@ class Stream(commands.Cog):
             # Get access token
             token = await self.get_access_token(session)
 
-            # Get stream info
+            # Get stream info to check if live
             async with session.get(f'https://api.twitch.tv/helix/streams?user_login={self.config["STREAMER_NAME"]}', headers={
                 'Client-ID': self.config["TWITCH_CLIENT_ID"],
                 'Authorization': f'Bearer {token}'
@@ -33,26 +36,42 @@ class Stream(commands.Cog):
                         user_data = await user_response.json()
                         user_info = user_data['data'][0] if user_data['data'] else None
 
-                        print(data['data'][0])
-
                         if user_info:
+                            # Download thumbnail first
+                            thumbnail_url = data['data'][0]['thumbnail_url'].replace('{width}x{height}', '1920x1080')
+                            await self.download_thumbnail(thumbnail_url)
+
+                            stream_info = data['data'][0]
+
                             embed = Embed(
-                                title=self.config["STREAMER_NAME"],
+                                title=stream_info['title'],
                                 color=0x9146FF
                             )
 
-                            stream_info = data['data'][0]
-                            embed.add_field(name="", value=f"[{stream_info['title']}](https://www.twitch.tv/{self.config["STREAMER_NAME"]})", inline=False)
-                            embed.set_image(url=stream_info['thumbnail_url'].replace('{width}x{height}', '1920x1080'))
-                            embed.set_thumbnail(url=user_info['profile_image_url'])
-                            embed.timestamp = datetime.datetime.now()
+                            embed.add_field(
+                                name="",
+                                value=f"""
+                                [{stream_info['user_name']}](https://www.twitch.tv/{stream_info['user_login']})
+                                {stream_info['game_name']}
+                                """,
+                                inline=False
+                            )
 
-                            channel = self.bot.get_channel(int(self.config["GOING_LIVE_CHANNEL_ID"]))
-                            if channel:
-                                self.stream_message = await channel.send(f'{self.config["STREAMER_NAME"]} is now live! <https://www.twitch.tv/{self.config["STREAMER_NAME"]}> @everyone', embed=embed)
-                                self.alreadyLive = True
-                            else:
-                                print(f"Channel with ID {self.config['GOING_LIVE_CHANNEL_ID']} not found.")
+                            # Set thumbnail from file attachment
+                            if os.path.exists(self.thumbnail_path):
+                                file = File(self.thumbnail_path, filename="twitch_thumbnail.png")
+                                embed.set_image(url="attachment://twitch_thumbnail.png")
+                                embed.set_thumbnail(url=user_info['profile_image_url'])
+
+                                self.stream_start_time = datetime.datetime.now()
+                                embed.timestamp = self.stream_start_time
+
+                                channel = self.bot.get_channel(int(self.config["GOING_LIVE_CHANNEL_ID"]))
+                                if channel:
+                                    self.stream_message = await channel.send(f'{self.config["STREAMER_NAME"]} is now live! <https://www.twitch.tv/{self.config["STREAMER_NAME"]}> @everyone', embed=embed, file=file)
+                                    self.alreadyLive = True
+                                else:
+                                    print(f"Channel with ID {self.config['GOING_LIVE_CHANNEL_ID']} not found.")
                         else:
                             print("Could not get user info")
                 elif data['data'] and self.alreadyLive:
@@ -61,6 +80,9 @@ class Stream(commands.Cog):
                 else:
                     self.alreadyLive = False
                     self.stream_message = None
+                    # Clean up thumbnail file
+                    if os.path.exists(self.thumbnail_path):
+                        os.remove(self.thumbnail_path)
 
     # When stream is live, update thumbnail every 6 minutes since Twitch updates the thumbnail every 5
     @tasks.loop(minutes=6)
@@ -69,6 +91,7 @@ class Stream(commands.Cog):
             async with aiohttp.ClientSession() as session:
                 token = await self.get_access_token(session)
 
+                # Get stream info to check if live
                 async with session.get(f'https://api.twitch.tv/helix/streams?user_login={self.config["STREAMER_NAME"]}', headers={
                     'Client-ID': self.config["TWITCH_CLIENT_ID"],
                     'Authorization': f'Bearer {token}'
@@ -86,26 +109,52 @@ class Stream(commands.Cog):
                             user_info = user_data['data'][0] if user_data['data'] else None
 
                             if user_info:
-                                # Update embed with new thumbnail
+                                # Download new thumbnail
+                                thumbnail_url = stream_info['thumbnail_url'].replace('{width}x{height}', '1920x1080')
+                                await self.download_thumbnail(thumbnail_url)
+
+                                # Update embed with new metadata
                                 embed = Embed(
-                                    title=self.config["STREAMER_NAME"],
+                                    title=stream_info['title'],
                                     color=0x9146FF
                                 )
 
-                                embed.add_field(name="", value=f"[{stream_info['title']}](https://www.twitch.tv/{self.config["STREAMER_NAME"]})", inline=False)
-                                embed.set_image(url=stream_info['thumbnail_url'].replace('{width}x{height}', '1920x1080'))
-                                embed.set_thumbnail(url=user_info['profile_image_url'])
-                                embed.timestamp = datetime.datetime.now()
+                                embed.add_field(
+                                    name="",
+                                    value=f"""
+                                    [{stream_info['user_name']}](https://www.twitch.tv/{stream_info['user_login']})
+                                    {stream_info['game_name']}
+                                    """,
+                                    inline=False
+                                )
 
-                                # Attempt to edit embed with new metadata
-                                try:
-                                    await self.stream_message.edit(embed=embed)
-                                except:
-                                    print("There was an error updating the stream message with new metadata.")
+                                # Grab newly downloaded thumbnail and update the embed
+                                if os.path.exists(self.thumbnail_path):
+                                    file = File(self.thumbnail_path, filename="twitch_thumbnail.png")
+                                    embed.set_image(url="attachment://twitch_thumbnail.png")
+                                    embed.set_thumbnail(url=user_info['profile_image_url'])
+                                    embed.timestamp = self.stream_start_time
+
+                                    try:
+                                        await self.stream_message.edit(embed=embed, attachments=[file])
+                                    except Exception as e:
+                                        print("There was an error updating the stream message with new metadata.")
+                                        print(f"{e}")
                     else:
                         # Stream ended, stop updating
                         self.alreadyLive = False
                         self.stream_message = None
+
+    async def download_thumbnail(self, url):
+        """Download thumbnail from URL and save it locally since direct link doesn't always update immediately on Discord"""
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        with open(self.thumbnail_path, 'wb') as f:
+                            f.write(await response.read())
+        except Exception as e:
+            print(f"Error downloading thumbnail: {e}")
 
     async def get_access_token(self, session):
         async with session.post('https://id.twitch.tv/oauth2/token', params={
